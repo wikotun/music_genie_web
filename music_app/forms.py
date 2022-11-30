6587f95648b9d1c2d9b16489b31c97f3e824d832
Wole Ikotun <wolkott@gmail.com>
2022-11-29 20:18:08 -0500

    minor updates

--- music_app/forms.py
+++ music_app/forms.py
@@ -2,16 +2,15 @@ from django.forms import ModelForm
 from django.forms import ModelForm, TextInput, NumberInput,FileInput
 from .models import Artist, Song
 from crispy_forms.helper import FormHelper
+from crispy_forms.layout import (
+    Column, Layout, Row,
+)
 
 
 class ArtistForm(ModelForm):
     class Meta:
         model = Artist
         fields = "__all__"
-
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.helper = FormHelper()
 
         widgets = {
             'name': TextInput(attrs={'required': True, 'class': "form-control", 'placeholder': 'Artist Name'}),
@@ -21,6 +20,18 @@ class ArtistForm(ModelForm):
             'label': TextInput(attrs={'class': "form-control", 'placeholder': 'Record Label'}),
             'image': FileInput(attrs ={'class':"img-thumbnail"})
         }
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column(
+                    "image", css_class="img-thumbnail",
+                ),
+            ),
+        )
+
 
 
 class SongForm(ModelForm):
